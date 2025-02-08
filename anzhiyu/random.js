5d0e8a0cd57eac8955db4b37accbdb6894c8bf52
Atra <atraa0621@gmail.com>
2025-02-08 11:34:40 +0800

    Site updated: 2025-02-08 11:34:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/auto-green/","2025/02/06/cursor/","2025/02/06/hello-world/","2025/02/06/hexo/"];function toRandomPost(){
+var posts=["2025/02/06/cursor/","2025/02/06/hexo/","2025/02/06/hello-world/","2025/02/07/auto-green/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
