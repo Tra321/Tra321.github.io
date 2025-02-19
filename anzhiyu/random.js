f23768b8894fb2949ad9ac6c314177ecce120b89
Atra <atraa0621@gmail.com>
2025-02-19 22:44:01 +0800

    Site updated: 2025-02-19 22:43:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/04/2025-02-04-使用-Github-Pages-和-Hexo-搭建自己的独立博客/","2025/02/06/2025-02-06-Cursor的无限次使用方法/","2025/02/08/2025-02-08-自动点亮绿色小方块的git-push机器人/","2025/02/10/2025-02-10-Redis的基础使用命令/","2025/02/17/2025-2-17-Docker/"];function toRandomPost(){
+var posts=["2025/02/04/2025-02-04-使用-Github-Pages-和-Hexo-搭建自己的独立博客/","2025/02/06/2025-02-06-Cursor的无限次使用方法/","2025/02/10/2025-02-10-Redis的基础使用命令/","2025/02/08/2025-02-08-自动点亮绿色小方块的git-push机器人/","2025/02/16/2025-02-16-Docker保姆级安装教程/","2025/02/19/2025-02-20-使用Docker-Compose一键部署前后端分离项目/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
