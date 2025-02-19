4a37200a1b3f548046fd9c7b48e24db310b4b662
Atra <atraa0621@gmail.com>
2025-02-19 17:02:33 +0800

    Site updated: 2025-02-19 17:02:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/17/2025-2-17-Docker/","2025/02/08/Redis/","2025/02/07/auto-green/","2025/02/06/hello-world/","2025/02/06/hexo/","2025/02/06/cursor/"];function toRandomPost(){
+var posts=["2025/02/17/2025-2-17-Docker/","2025/02/05/2025-2-5-Hexo/","2025/02/06/2025-2-6-Cursor/","2025/02/07/2025-2-7-Auto-green/","2025/02/08/2025-2-8-Redis/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
